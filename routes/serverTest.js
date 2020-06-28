e0a1b36b5cfdd8eb1355f09ed58585f595728537
Mateusz Mądel <mateuszm7234@gmail.com>
2020-06-28 17:14:10 +0200

    fix

--- routes/serverTest.js
+++ routes/serverTest.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
-
+const CoinpaprikaAPI = require('@coinpaprika/api-nodejs-client');
+const client = new CoinpaprikaAPI();
 router.get('/',async (req,res)=>{
-    res.send('online')
+    try {
+        await client.getGlobal()
+        res.status(200).send('online')
+    }
+    catch (e) {
+        res.status(400).send(e.message)
+    }
 })
 
 module.exports= router;
