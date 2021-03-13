b6bad5f83328753c8069d6a96b084d3000d44898
Mateusz Mądel <mateuszm7234@gmail.com>
2021-03-14 00:52:59 +0100

    List filtering removed

--- routes/list.js
+++ routes/list.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', async (req,res)=>{
-    let list=data.filter((item) => item.rank <= 100).sort((a, b) => a.rank < b.rank ? -1 : 1).map(({id, name}) => ({id, name}))
+    let list=data.sort((a, b) => a.rank < b.rank ? -1 : 1).map(({id, name}) => ({id, name}))
     if(req.query.all!==undefined)
     list.unshift({id:'USD', name:'Dolar amerykański' },{id:'PLN',name:'Polski złoty'},{id:'EUR',name:'Euro'},{id:'GBP',name:'Funt brytyjski'})
     res.json(list)
